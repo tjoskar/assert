53a878af42e885ea2ffbe7b3afe07ac25838fb53
tjoskar <hello@tjoskar.dev>
2022-07-18 09:50:18 +0200

    chore: Using esm
    Dropping support for node 12 and using esm

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const equal = (a, b) => a === b
+const equal = (a, b) => a === b;
 
-function is(value, expected, message) {
-  if (!equal(value, expected)) {
-    throw new Error(message ? message : `${value} is not equal to ${expected}`)
-  }
+export function is(value, expected, message) {
+	if (!equal(value, expected)) {
+		throw new Error(message ? message : `${value} is not equal to ${expected}`);
+	}
 }
 
-function isNot(value, expected, message) {
-  if (equal(value, expected)) {
-    throw new Error(message ? message : `${value} is equal to ${expected}`)
-  }
+export function isNot(value, expected, message) {
+	if (equal(value, expected)) {
+		throw new Error(message ? message : `${value} is equal to ${expected}`);
+	}
 }
-
-module.exports = { is, isNot }
-
--- index.test.js
+++ index.test.js
@@ -1,21 +1,22 @@
-const { is, isNot } = require('./')
+import {test, expect} from 'vitest';
+import {is, isNot} from './index.js';
 
-test(`true is true`, () => {
-  expect(() => is(true, true)).not.toThrow()
-})
+test('true is true', () => {
+	expect(() => is(true, true)).not.toThrow();
+});
 
-test(`false is false`, () => {
-  expect(() => is(false, false)).not.toThrow()
-})
+test('false is false', () => {
+	expect(() => is(false, false)).not.toThrow();
+});
 
-test(`Throw an error for comparing false and true`, () => {
-  expect(() => is(true, false)).toThrow('true is not equal to false')
-})
+test('Throw an error for comparing false and true', () => {
+	expect(() => is(true, false)).toThrow('true is not equal to false');
+});
 
-test(`true is not false`, () => {
-  expect(() => isNot(true, false)).not.toThrow()
-})
+test('true is not false', () => {
+	expect(() => isNot(true, false)).not.toThrow();
+});
 
-test(`Throw an error for comparing false and false`, () => {
-  expect(() => isNot(false, false)).toThrow('false is equal to false')
-})
+test('Throw an error for comparing false and false', () => {
+	expect(() => isNot(false, false)).toThrow('false is equal to false');
+});
